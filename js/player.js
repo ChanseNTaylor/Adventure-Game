b205c36ce13067edb7322dd98cacb4b4f499837b
Chanse Taylor <Chansen.taylor@gmail.com>
2019-10-21 16:36:41 -0400

    Added semicolons to the methods of the player.js file.  They are not strictly necessary, but I feel that they are important to include.  Added a method for adding items to the players inventory.

--- js/player.js
+++ js/player.js
@@ -5,23 +5,28 @@ const Player = function()
     let moves = 0;
     let maxHP = 5;
     let currentHP = 5;
-    let inventory = [];
     let rank = "Beginner";
     let location = "Bad Place";
 
+    const inventory = [];
+
     return {
-        setScore(num = 0) { score += num },
-        setMoves(num = 0) { moves += num },
-        setCurrentHP(num = 0) { currentHP += num },
-        setMaxHP(num = maxHP) { maxHP += num },
-        setRank(newRank = rank) { rank = newRank },
-        setLocation(place = location) { location = place },
-        getScore() { return score },
-        getMoves() { return moves },
-        getCurrentHP() { return currentHP },
-        getMaxHP() { return maxHP },
-        getRank() { return rank },
-        getLocation() { return location },
-        getInventory() { return inventory }
+        // setters
+        setScore(num = 0) { score += num; },
+        setMoves(num = 0) { moves += num; },
+        setCurrentHP(num = 0) { currentHP += num; },
+        setMaxHP(num = maxHP) { maxHP += num; },
+        setRank(newRank = rank) { rank = newRank; },
+        setLocation(place = location) { location = place; },
+        // getters
+        getScore() { return score; },
+        getMoves() { return moves; },
+        getCurrentHP() { return currentHP; },
+        getMaxHP() { return maxHP; },
+        getRank() { return rank; },
+        getLocation() { return location; },
+        getInventory() { return inventory; },
+        // utility
+        addToInventory(item) { inventory.push(item); }
     }
 }
