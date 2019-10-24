835b4ad0c1a2ee46ad2ce04e8b1205d4a0e29ea2
Chanse Taylor <Chansen.taylor@gmail.com>
2019-10-24 00:27:14 -0400

    Made minor changes to the html for clarity. Added code to UI.js to display user input.  This is the start of properly handling user input and generating an appropriate response.  Changes in styles.css to style user input.

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,25 @@
 "use strict";
 const UI = function()
 {
+    const input = document.getElementsByClassName("input")[0];
     const textDiv = document.getElementsByClassName("textDiv")[0];  // fix name
     const time = document.getElementsByClassName("header__time")[0];
     const moves = document.getElementsByClassName("header__moves")[0];
     const score = document.getElementsByClassName("header__score")[0];
     const location = document.getElementsByClassName("header__location")[0];
+
+    textDiv.addEventListener("click", evt => { input.focus(); });
+
+    input.addEventListener("keydown", evt =>
+    {
+        if(evt.key == "Enter")
+        {
+            parseUserInput(input.value);
+            addInputText(`>${input.value}`);
+            
+            input.value = "";
+        }
+    });
 
     const addTitleText = text =>
     {
@@ -19,13 +33,25 @@ const UI = function()
 
     const addSubtitleText = text =>
     {
+        const newParagraph = document.createElement("P");
+
+        newParagraph.appendChild(document.createTextNode(text));
+        newParagraph.classList.add("textDiv__message");
+
+        textDiv.appendChild(newParagraph);
+    }
+
+    const addInputText = text =>
+    {
         const newSpan = document.createElement("SPAN");
 
         newSpan.appendChild(document.createTextNode(text));
-        newSpan.classList.add("textDiv__message");
+        newSpan.classList.add("textDiv__input");
 
         textDiv.appendChild(newSpan);
     }
+
+    const addResponseText = () => {};
 
     return {
         addTitleText: addTitleText,
